fb0baedc72d2c334e9e7235b891a27c2c7435670
0xNaN <enzodicicco@gmail.com>
2015-11-09 10:59:51 +0100

    some refactoring, extracting getNumbersFrom(), getLines(), ..

--- src/string_calculator_kata/StringCalculator.java
+++ src/string_calculator_kata/StringCalculator.java
@@ -5,32 +5,38 @@ import java.util.List;
 
 public class StringCalculator {
 
+	private static final String NEW_LINE_SEQUENCE = "\n";
 	private static final String EMTY_LINE = "";
 	private static final String SEPARATOR = ",";
 
 	public int add(String numbers) {
 		int total = 0;
-		for(String line: numbers.split("\n")) {
-			if(isEmpty(line))
-				total += 0;
-			else
+		for(String line: getLines(numbers)) {
 				total += computeLine(line);
 		}
 		return total;
    }
 
-	private boolean isEmpty(String numbers) {
-		return EMTY_LINE.equals(numbers);
-	}
-
-	private int computeLine(String numbers) {
-		int total = 0;
+	private int computeLine(String line) {
+		if(isEmpty(line))
+			return 0;
 		
-		List<String> nums = Arrays.asList(numbers.split(SEPARATOR));
-		for(String number: nums) 
+		int total = 0;
+		for(String number: getNumbersFrom(line)) 
 			total += Integer.parseInt(number);
-	
+		
 		return total;
 	}
 
+	private List<String> getNumbersFrom(String lineNumbers) {
+		return Arrays.asList(lineNumbers.split(SEPARATOR));
+	}
+
+	private String[] getLines(String data) {
+		return data.split(NEW_LINE_SEQUENCE);
+	}
+
+	private boolean isEmpty(String line) {
+		return EMTY_LINE.equals(line);
+	}
 }
