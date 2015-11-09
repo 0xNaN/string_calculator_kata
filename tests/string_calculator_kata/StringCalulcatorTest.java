6a61de7b3c1bbf13cd4cc078919419ac3c7f90e1
0xNaN <enzodicicco@gmail.com>
2015-11-09 09:52:27 +0100

    refactoring tests

--- tests/string_calculator_kata/StringCalulcatorTest.java
+++ tests/string_calculator_kata/StringCalulcatorTest.java
@@ -2,21 +2,26 @@ package string_calculator_kata;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalulcatorTest {
+	StringCalculator sc;
+	
+	@Before
+	public void setUp() {
+		sc = new StringCalculator();	
+	}
 	
 	@Test
 	public void
 	should_return_zero_for_empty_string() {
-		StringCalculator sc = new StringCalculator();
 		assertEquals(0, sc.add(""));
 	}
 	
 	@Test
 	public void
 	should_return_one_for_a_string_with_only_one() {
-		StringCalculator sc = new StringCalculator();
 		assertEquals(1, sc.add("1"));
 	}
 }
