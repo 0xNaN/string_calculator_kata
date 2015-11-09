86e9e3978c9a3e2d81d762349724a6bddc7c9bfa
0xNaN <enzodicicco@gmail.com>
2015-11-09 14:17:59 +0100

    refactoring

--- src/string_calculator_kata/StringCalculator.java
+++ src/string_calculator_kata/StringCalculator.java
@@ -6,95 +6,92 @@ import java.util.List;
 
 public class StringCalculator {
 
-	private static final String SEPARATOR_PATTERN = "//.*\n";
-	private static final String SEPARATOR_HEADER_PREFIX = "//";
-	private static final String NEW_LINE_SEQUENCE = "\n";
-	private static final String EMTY_LINE = "";
+	private static final String SEPARATOR_PREFIX = "//";
 	private static final String DEFAULT_SEPARATOR = ",";
+	private static final String SEPARATOR_PATTERN = SEPARATOR_PREFIX + ".*";
+	private static final String NEW_LINE_SEQUENCE = "\n";
 
 	public int add(String numbers) throws NegativeNotAllowed {
 		int total = 0;
-		for(Integer num: getNumbersIfPositive(numbers))
+		for(Integer num: positiveNumbersFrom(numbers))
 			total += num;
 		return total;
 	}
 
-
-	private List<Integer> getNumbersIfPositive(String numbers) throws NegativeNotAllowed {
-		List<Integer> allNumbers = getNumbersFrom(numbers);
-		checkPositivness(allNumbers);
+	private List<Integer> positiveNumbersFrom(String numbers) throws NegativeNotAllowed {
+		List<Integer> allNumbers = numbersFrom(numbers);
+		checkPositiveness(allNumbers);
 		return allNumbers;
 	}
+	
 
-
-	private List<Integer> getNumbersFrom(String data) {
-		List<String> lines = getLines(data);
-		String separator = getSeparator(lines.get(0));
-
-		if(isSeparatorHeader(lines.get(0)))
+	private List<Integer> numbersFrom(String data) {
+		List<String> lines = linesFrom(data);	
+		
+		String separatorHeader = lines.get(0);
+		if(isSeparatorHeader(separatorHeader))
 			lines.remove(0);
 		
-		return getNumbersFrom(collapseToALine(data, separator), separator);
+		String separator = separatorFrom(separatorHeader);
+		return numbersFrom(collapseToALine(data, separator), separator);
 	}
+	
 
-
-
-	private List<Integer> getNumbersFrom(String line, String separator) {
+	private List<Integer> numbersFrom(String line, String separator) {
 		List<Integer> numbers = new LinkedList<Integer>();	
 		List<String> stringNumbers = splitNumbers(line, separator);
 		
 		for(String num: stringNumbers)
 			numbers.add(stringToNumber(num));
-		
+
 		return numbers;
 	}
-
+	
 
 	private List<String> splitNumbers(String line, String separator) {
 		return Arrays.asList(line.split(separator));
 	}
+	
 
-	private void checkPositivness(List<Integer> numbers) throws NegativeNotAllowed {
+	private void checkPositiveness(List<Integer> numbers) throws NegativeNotAllowed {
 		List<Integer> negativeNumbers = new LinkedList<Integer>();
 		for(Integer n: numbers) {
 			if(n < 0)
 				negativeNumbers.add(n);
 		}
-		if(negativeNumbers.size() > 0)
+		if(! negativeNumbers.isEmpty())
 			throw new NegativeNotAllowed(negativeNumbers.toString());
 	}
+	
 
-
-	private String getSeparator(String line) {
-		String separator = DEFAULT_SEPARATOR;
-		if(isSeparatorHeader(line)) {
-			separator = parseSeparator(line);
-		}
-		return separator;
-	}
-
-	private boolean isSeparatorHeader(String line) {
-		return line.startsWith(SEPARATOR_HEADER_PREFIX);
-	}
-
-	private String parseSeparator(String line) {
-		return line.substring(SEPARATOR_HEADER_PREFIX.length());
-	}
-
-	private String collapseToALine(String data, String separator) {
-		data = data.replaceFirst(SEPARATOR_PATTERN, "");
-		data = data.replace(NEW_LINE_SEQUENCE, separator);
-		return data;
-	}
-
-	private List<String> getLines(String data) {
-		return new LinkedList(Arrays.asList(data.split(NEW_LINE_SEQUENCE)));
-	}
-
-	private boolean isEmpty(String line) {
-		return EMTY_LINE.equals(line);
+	private String separatorFrom(String line) {
+		return isSeparatorHeader(line)
+				? parseSeparator(line)
+				: DEFAULT_SEPARATOR;
 	}
 	
+
+	private boolean isSeparatorHeader(String line) {
+		return line.matches(SEPARATOR_PATTERN);
+	}
+	
+
+	private String parseSeparator(String line) {
+		return line.substring(SEPARATOR_PREFIX.length());
+	}
+	
+
+	private String collapseToALine(String data, String separator) {
+		return data.replaceFirst(SEPARATOR_PATTERN, "")
+				.replace(NEW_LINE_SEQUENCE, separator);
+	}
+	
+
+	private List<String> linesFrom(String data) {
+		return new LinkedList<String>(Arrays.asList(data.split(NEW_LINE_SEQUENCE)));
+	}
+	
+
 	private Integer stringToNumber(String num) {
 		return "".equals(num) ? 0 : Integer.parseInt(num);
 	}
